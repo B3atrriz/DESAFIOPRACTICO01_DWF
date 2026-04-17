7d57f63fda6b8e0587c284035a5775ea45e5ed8e
B3atrriz <elisabetgonzalez2007@gmail.com>
2026-04-17 17:49:32 -0600

    add: dependencias para MySQL y Swagger OpenAPI

--- Documents/DESAFIO1_DWF/Sistema_Escolar/src/main/java/sv/edu/udb/repository/AlumnoMateriaRepository.java
+++ Documents/DESAFIO1_DWF/Sistema_Escolar/src/main/java/sv/edu/udb/repository/AlumnoMateriaRepository.java
@@ -1,4 +1,4 @@
-package sv.edu.udb.repository;
+https://github.com/B3atrriz/DESAFIOPRACTICO01_DWFpackage sv.edu.udb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
